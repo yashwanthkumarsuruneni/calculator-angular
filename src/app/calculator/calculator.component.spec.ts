24398ccdf8cb65954fd85ee75da34c9c8577eb14
Yashwanth Suruneni <yashwanthsuruneni@gmail.com>
2018-06-01 12:54:48 -0400

    added tests

--- src/app/calculator/calculator.component.spec.ts
+++ src/app/calculator/calculator.component.spec.ts
@@ -1,4 +1,7 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+
 
 import { CalculatorComponent } from './calculator.component';
 
@@ -8,7 +11,10 @@ describe('CalculatorComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CalculatorComponent ]
+      declarations: [ CalculatorComponent ],
+      imports: [
+        FormsModule
+      ]
     })
     .compileComponents();
   }));
@@ -19,7 +25,56 @@ describe('CalculatorComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the calculator component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should be initialized "operation" ', () => {
+    const calculator = component;
+    expect(calculator.operation).toBeDefined();
+  });
+  it('display should be null ', () => {
+    const calculator = component;
+    expect(calculator.display).toBe('');
+  });
+  it('activenumber should be null ', () => {
+    const calculator = component;
+    expect(calculator.activenumber).toBe('');
+  });
+  it('the method number should display the number passed as param' , () => {
+   const calculator = component;
+   calculator.number('5');
+   expect(calculator.display).toBe('5');
+  });
+  it('the operator should throw an error if called directly' , () => {
+    const calculator = component;
+    calculator.operator('x');
+    expect(calculator.display).toBe('error!');
+  });
+  it('the operator should throw an error if called directly' , () => {
+    const calculator = component;
+    calculator.number('5');
+    calculator.operator('x');
+    expect(calculator.display).not.toBe('error!');
+  });
+  it('test whether the DOM is diplaying the number entered', fakeAsync(() => {
+    const inputDe = fixture.debugElement.query(By.css('input[name="number"]'));
+    const inputEl = inputDe.nativeElement;
+    const calculator = component;
+    calculator.number('5');
+    fixture.detectChanges();
+    tick();
+    expect(inputEl.value).toBe('5');
+  }));
+  it('test whether the DOM is clearing the number entered', fakeAsync(() => {
+    const inputDe = fixture.debugElement.query(By.css('input[name="number"]'));
+    const inputEl = inputDe.nativeElement;
+    const calculator = component;
+    calculator.number('5');
+    calculator.clear();
+    fixture.detectChanges();
+    tick();
+    expect(inputEl.value).toBe('');
+  }));
+
 });
